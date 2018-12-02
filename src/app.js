592ae99fd9f156c67603c7469f5e07f2f07432ef
ayming <ayming1011@gmail.com>
2018-12-02 19:47:09 +0800

    add helmet to secure apps by setting various HTTP headers

--- src/app.js
+++ src/app.js
@@ -1,13 +1,18 @@
 import express from 'express'
 import compression from 'compression'
 import bodyParser from 'body-parser'
+import helmet from 'helmet'
 import errors from './errors'
 
 const app = express()
 
 app.use(compression())
+// parse body params and attache them to req.body
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
+
+// secure apps by setting various HTTP headers
+app.use(helmet())
 
 app.get('/', (req, res) => {
   // throw new Error('Test Error')
